bd84f3e9c58e2246f9617e79a0c5943cc18323bb
Shoshin Oleg <shoshin1997@yandex.ru>
2021-12-31 19:43:07 +0300

    Some changes

--- src/main/kotlin/com/shoshin/models/schedule/WeekSchedule.kt
+++ src/main/kotlin/com/shoshin/models/schedule/WeekSchedule.kt
@@ -4,5 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class WeekSchedule (
-    val days: List<DayWeekSchedule>
+    val days: List<DayWeekSchedule> = listOf()
 )
--- src/main/kotlin/com/shoshin/routes/schedule/GetScheduleRoute.kt
+++ src/main/kotlin/com/shoshin/routes/schedule/GetScheduleRoute.kt
@@ -3,6 +3,7 @@ package com.shoshin.routes.schedule
 import com.shoshin.common.Reaction
 import com.shoshin.common.default_responses.internalServerError
 import com.shoshin.common.default_responses.ok
+import com.shoshin.models.schedule.WeekSchedule
 import io.ktor.application.*
 import io.ktor.routing.*
 
@@ -10,10 +11,7 @@ fun Route.getDefaultScheduleRoute() {
     get("schedules/default") {
         when(val result = ScheduleRepo.getDefaultSchedule()) {
             is Reaction.Error -> return@get call.internalServerError()
-            is Reaction.Success -> {
-                println("result=${result.data}")
-                return@get call.ok(result.data)
-            }
+            is Reaction.Success -> return@get call.ok(result.data)
         }
     }
 }
