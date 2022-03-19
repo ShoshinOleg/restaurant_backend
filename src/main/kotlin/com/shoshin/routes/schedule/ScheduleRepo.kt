94f165f46527df33cff294c50bccc58f4ea4ccce
Shoshin Oleg <shoshin1997@yandex.ru>
2022-03-19 17:59:05 +0300

    Some changes

--- src/main/kotlin/com/shoshin/routes/schedule/ScheduleRepo.kt
+++ src/main/kotlin/com/shoshin/routes/schedule/ScheduleRepo.kt
@@ -43,17 +43,24 @@ class ScheduleRepo {
                 REF_SCHEDULE.child("default")
                     .addListenerForSingleValueEvent(object: ValueEventListener {
                         override fun onDataChange(snapshot: DataSnapshot?) {
+                            println("onDataChange")
+                            println("snapshot=$snapshot")
                             if(snapshot == null) {
                                 throw NotFoundException("Default schedule not found")
                             } else {
                                 var schedule = snapshot.getValue(WeekSchedule::class.java)
+                                println("schedule=$schedule")
                                 schedule = schedule ?: WeekSchedule()
                                 continuation.resume(schedule)
                             }
                         }
 
-                        override fun onCancelled(error: DatabaseError?) =
+                        override fun onCancelled(error: DatabaseError?)  {
+                            println("error")
+                            println("error=$error")
                             throw error?.toException() ?: Throwable(error?.message, error?.toException()?.cause)
+                        }
+
                     })
             }
     }
