91f8004334988a0a2db3b80971ed95052d1a71e6
Shoshin Oleg <shoshin1997@yandex.ru>
2022-03-22 21:39:22 +0300

    Add route "get orders metadata"

---
+++ src/main/kotlin/com/shoshin/routes/orders/GetOrdersMetadataRoute.kt
@@ -0,0 +1,17 @@
+package com.shoshin.routes.orders
+
+import com.shoshin.common.default_responses.internalServerError
+import com.shoshin.common.default_responses.ok
+import com.shoshin.firebase.FirebasePrincipal
+import com.shoshin.routes.users.UsersRepo
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.routing.*
+
+fun Route.getOrdersMetadataRoute() {
+    get("orders/metadata") {
+        val principal = call.principal<FirebasePrincipal>() ?: return@get call.internalServerError()
+        val ordersMetadata = UsersRepo.getOrdersMetadata(principal.userId)
+        return@get call.ok(ordersMetadata)
+    }
+}
--- src/main/kotlin/com/shoshin/routes/orders/OrderRoutes.kt
+++ src/main/kotlin/com/shoshin/routes/orders/OrderRoutes.kt
@@ -7,6 +7,7 @@ import io.ktor.routing.*
 fun Application.registerOrderRoutes() {
     routing {
         authenticate("firebase") {
+            getOrdersMetadataRoute()
             makeOrder()
         }
     }
