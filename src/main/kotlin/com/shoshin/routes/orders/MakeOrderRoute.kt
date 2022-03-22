05a0f35b42f7d80040e5afbd38a02bdd52f96ca2
Shoshin Oleg <shoshin1997@yandex.ru>
2022-03-22 19:13:05 +0300

    Some changes

--- src/main/kotlin/com/shoshin/routes/orders/MakeOrderRoute.kt
+++ src/main/kotlin/com/shoshin/routes/orders/MakeOrderRoute.kt
@@ -14,21 +14,12 @@ import io.ktor.routing.*
 fun Route.makeOrder() {
     post("orders") {
         val principal = call.principal<FirebasePrincipal>() ?: return@post call.internalServerError()
-
-        println("o1")
-        println("call.request = ${call.request}")
-//        call.response
         val order = call.receive<Order>()
-        println("o2")
         if(principal.userId != order.customerId) {
-            println("o3")
             return@post call.forbidden()
         } else {
-            println("o4")
             order.id = OrdersRepo.newId()
-            println("o5")
             OrdersRepo.updateOrder(order)
-            println("o6")
             return@post call.ok(order.getOrderMetaData())
         }
     }
